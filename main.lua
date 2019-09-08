610c8a73b9d2355571be379921697e5830f4682f
Allerek <allerek22@interia.pl>
2019-09-08 20:01:06 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,181 +1,224 @@
-window = {
-    options={}
-}
+local check
+if fileExists("update.cfg") then
+	check = fileOpen("update.cfg")
+else
+	check = fileCreate("update.cfg")
+end
+local allstr = fileRead(check,fileGetSize(check))
+setElementData(resourceRoot,"Version",allstr)
+fileClose(check)
 
-local sound
-local actual = 1
-isNativeShown = false
-switch={}
-
-function createNativeUI(name,title,image,color,namecolor,titlecolor,align,counter)
-    if not name == nil and not name == "" then
-        assert(type(name) == string,"Bad argument @ createNativeUI [expected string at argument 1, got "..type(name).." '"..name.."'']")
-    end
-    if not title then
-        title = "Native UI"
-    end
-    assert(title and tostring(title),"Bad argument @ createNativeUI [expected string at argument 2, got "..type(title).." '"..title.."'']")
-    if image == nil and not color then return end
-    if not align then 
-        align = "right"
-    end
-    assert(align == "left" or align == "right","Invalid align type @ createNativeUI [expected 'left'/'right' at argument 7, got"..type(align).." '"..align.."'']")
-    window = {}
-    window.items={}
-    zoom = getZoom()
-    window.name = name
-    window.title = title
-    window.scale = scaleScreen(10,10,431,107,align,"top")
-    window.namepos = Vector2(unpack(window.scale,1),unpack(window.scale,2)) 
-    window.namesize = Vector2(unpack(window.scale,3),unpack(window.scale,4))
-    window.namepos2 = Vector2(window.namepos["x"]+(window.namesize["x"]/2),window.namepos["y"]+(window.namesize["y"]/2))
-    window.scale = scaleScreen(0,0,431,37,"left","top")
-    window.titlepos = Vector2(window.namepos["x"],window.namepos["y"]+window.namesize["y"])
-    window.titlesize = Vector2(unpack(window.scale,3),unpack(window.scale,4))
-    window.titlepos2 = Vector2(window.titlepos["x"]+(15/zoom),window.titlepos["y"]+(window.titlesize["y"]/2))
-    window.titlepos3 = Vector2(window.titlepos["x"]+window.titlesize["x"]-(15/zoom),window.titlepos["y"]+(window.titlesize["y"]/2))
-    window.namefont = dxCreateFont("assets/font.ttf",55/zoom)
-    window.titlefont = dxCreateFont("assets/fonttitle.ttf",18/zoom,false)
-    window.counter = counter or false
-    if not image then
-        window.image = "assets/defaultbg.png"
-    else
-        window.image = image
-    end
-    if not namecolor then
-        window.namecolor = tocolor(255,255,255)
-    else
-        window.namecolor = namecolor
-    end
-    if not titlecolor then
-        window.titlecolor = tocolor(255,255,255)
-    else
-        window.titlecolor = titlecolor
-    end
-    bindKeys()
-    isNativeShown = true
-    addEventHandler("onClientRender",getRootElement(),renderNative)
+Version = tonumber(allstr) or 0
+RemoteVersion = 0
+ManualUpdate = false
+updateTimer = false
+updatePeriodTimer = false
+function checkUpdate()
+	outputDebugString("[NativeUI]Connecting to github...")
+	fetchRemote("https://raw.githubusercontent.com/Allerek/MTASA-NativeUI/master/update.cfg",function(data,err)
+		if err == 0 then
+			RemoteVersion = tonumber(data)
+            if not ManualUpdate then
+				if RemoteVersion > Version then
+					outputDebugString("[NativeUI]Remote Version Got [Remote:"..data.." Current:"..allstr.."].")
+					outputDebugString("[NativeUI]Update? Command: updatenative")
+					if isTimer(updateTimer) then killTimer(updateTimer) end
+					updateTimer = setTimer(function()
+						if RemoteVersion > Version then
+							outputDebugString("[NativeUI]Remote Version Got [Remote:"..RemoteVersion.." Current:"..allstr.."].")
+							outputDebugString("[NativeUI]Update? Command: updatenative")
+						else
+							killTimer(updateTimer)
+						end
+					end,120*60000,0)
+				else
+					outputDebugString("[NativeUI]Current Version("..allstr..") is the latest!")
+				end
+			else
+				startUpdate()
+			end
+		else
+			outputDebugString("[NativeUI]Can't Get Remote Version ("..err..")")
+		end
+	end)
 end
 
-function addNativePlaceholder(text)
-    local table = {
-        ["type"] = "placeholder",
-        ["text"] = text
-    }
-    window.items[#window.items+1] = table
+
+function startUpdate()
+	ManualUpdate = false
+	setTimer(function()
+		outputDebugString("[NativeUI]Requesting Update Data (From github)...")
+		fetchRemote("https://raw.githubusercontent.com/Allerek/MTASA-NativeUI/master/meta.xml",function(data,err)
+			if err == 0 then
+				outputDebugString("[NativeUI]Update Data Acquired")
+				if fileExists("updated/meta.xml") then
+					fileDelete("updated/meta.xml")
+				end
+				local meta = fileCreate("updated/meta.xml")
+				fileWrite(meta,data)
+				fileClose(meta)
+				outputDebugString("[NativeUI]Requesting Verification Data...")
+				getGitHubTree()
+			else
+				outputDebugString("[NativeUI]!Can't Get Remote Update Data (ERROR:"..err..")",2)
+			end
+		end)
+	end,50,1)
 end
 
-function renderNative()
-    dxDrawImage(window.namepos,window.namesize,window.image)
-    dxDrawRectangle(window.titlepos,window.titlesize,tocolor(0,0,0))
-    dxDrawText(window.title,window.titlepos2,nil,nil,window.titlecolor,1,window.titlefont,"left","center")
-    if window.counter then
-        dxDrawText(actual.."/"..#window.items,window.titlepos3,nil,nil,window.titlecolor,1,window.titlefont,"right","center")
-    end
-    if not window.name == nil then
-        dxDrawText(window.name,window.namepos2,nil,nil,window.namecolor,1,window.namefont,"center","center")
-    end
-    for i,v in pairs(window.items) do
-        if i > 15 then return end        
-        local pos = Vector2(window.titlepos["x"],window.titlepos["y"]+window.titlesize["y"]+(window.titlesize["y"]*(i-1)))
-        local pos2 = Vector2(window.titlepos["x"]+(15/zoom),window.titlepos["y"]+window.titlesize["y"]/2+(window.titlesize["y"]*(i)))
-        local multiplier = 255*(0.03*i)
-        if actual == i then
-            color = tocolor(255,255,255,255*0.6)
-            textcolor = tocolor(0,0,0,255) 
-        else
-            color = tocolor(0,0,0,255-multiplier)
-            textcolor = tocolor(255,255,255)
-        end
-        dxDrawRectangle(pos,window.titlesize,color)
-        dxDrawText(v.text,pos2,nil,nil,textcolor,1,window.titlefont,"left","center")
-        if v.type == "switch" then
-            local pos2 = Vector2(window.titlepos["x"]+window.titlesize["x"]-(15/zoom),window.titlepos["y"]+window.titlesize["y"]/2+(window.titlesize["y"]*(i)))
-            local actual = tonumber(v.actual)
-            dxDrawText("⮜ "..v.value[actual].." ⮞",pos2,nil,nil,textcolor,1,window.titlefont,"right","center")
-        end
-    end
-end
 
-function removeNativeItem(id)
-    table.remove(window.items,id)
-end
-
-addEventHandler("onClientKey",getRootElement(),function(btn,state)
-    if not isNativeShown then return end 
-    if btn == "arrow_d" and state == true then
-        cancelEvent()
-    end
-    if btn == "arrow_u" and state == true then
-        cancelEvent()
-    end
-    if btn == "arrow_r" and state == true then
-        cancelEvent()
-    end
-    if btn == "arrow_l" and state == true then
-        cancelEvent()
-    end
-    if btn == "enter" and state == true then
-        cancelEvent()
-    end
+addCommandHandler("updatenative",function(player)
+	local account = getPlayerAccount(player)
+	local accName = getAccountName(account)
+	local isAdmin = isObjectInACLGroup("user."..accName,aclGetGroup("Admin")) or isObjectInACLGroup("user."..accName,aclGetGroup("Console"))
+	if isAdmin then
+		outputDebugString("[NativeUI]Player "..getPlayerName(player).." attempt to update native (Allowed)")
+		outputDebugString("[NativeUI]Preparing for updating native")
+		outputChatBox("[NativeUI]Preparing for updating native",root,0,255,0)
+        if RemoteVersion > Version then
+            print(RemoteVersion)
+            print(Version)
+			startUpdate()
+		else
+			ManualUpdate = true
+			checkUpdate()
+		end
+	else
+		outputChatBox("[NativeUI]Access Denined!",player,255,0,0)
+		outputDebugString("[NativeUI]Player "..getPlayerName(player).." attempt to update native (Denied)!",2)
+	end
 end)
 
 
-function bindKeys()
-    bindKey("arrow_d","up",function()
-        if not isNativeShown then return end
-        if actual+1 > #window.items then
-            actual = 1
-        else
-            actual = actual+1
-        end
-        playNativeSound()
-    end)
-    bindKey("arrow_u","up",function()
-        if not isNativeShown then return end
-        if actual == 0 then
-            actual = #window.items
-        else
-            actual = actual-1
-        end
-        
-        playNativeSound()
-    end)
-    bindKey("arrow_r","up",function()
-        if window.items[actual].type == "switch" then
-            window.items[actual].actual = window.items[actual].actual+1
-            if window.items[actual].actual > #window.items[actual].value then window.items[actual].actual = 1 end
-            playNativeSound()
-        end
-    end)
-    bindKey("arrow_l","up",function()
-        if window.items[actual].type == "switch" then
-            window.items[actual].actual = window.items[actual].actual-1
-            if window.items[actual].actual < 1 then window.items[actual].actual = #window.items[actual].value end
-            playNativeSound()
-        end
-    end)
-    bindKey("enter","up",function()
-        if window.items[actual].type == "switch" then
-            local actualswitch = window.items[actual].actual
-            local actualswitch = window.items[actual].value[tonumber(actualswitch)]
-            
-            triggerEvent("onClientAcceptSwitch",localPlayer,actual,actualswitch)
-            
-        end
-    end)
+addEventHandler("onResourceStart",resourceRoot,function()
+    checkUpdate()
+end)
+
+
+preUpdate = {}
+fileHash = {}
+preUpdateCount = 0
+UpdateCount = 0
+FetchCount = 0
+preFetch = 0
+folderGetting = {}
+function getGitHubTree(path,nextPath)
+	nextPath = nextPath or ""
+	fetchRemote(path or "https://api.github.com/repos/Allerek/MTASA-NativeUI/git/trees/master",function(data,err)
+		if err == 0 then
+			local theTable = fromJSON(data)
+			folderGetting[theTable.sha] = nil
+			for k,v in pairs(theTable.tree) do
+				if v.path ~= "meta.xml" then
+				local thePath = nextPath..(v.path)
+					if v.mode == "040000" then
+						folderGetting[v.sha] = true
+						getGitHubTree(v.url,thePath.."/")
+					else
+						fileHash[thePath] = v.sha
+					end
+				end
+			end
+			if not next(folderGetting) then
+				checkFiles()
+			end
+		else
+			outputDebugString("[NativeUI]Failed To Get Verification Data, Please Try Again Later (API Cool Down 60 mins)!",2)
+		end
+	end)
 end
 
-function playNativeSound()
-    if isElement(sound) then
-        destroyElement(sound)
-        sound = playSound("assets/change.wav",false)
-    else
-        sound = playSound("assets/change.wav",false)
-    end
+function checkFiles()
+	local xml = xmlLoadFile("updated/meta.xml")
+	for k,v in pairs(xmlNodeGetChildren(xml)) do
+		if xmlNodeGetName(v) == "script" or xmlNodeGetName(v) == "file" then
+			local path = xmlNodeGetAttribute(v,"src")
+			if --[[path ~= "meta.xml" and]] path ~= "update.cfg" then
+				local sha = ""
+				if fileExists(path) then
+					local file = fileOpen(path)
+					local size = fileGetSize(file)
+					local text = fileRead(file,size)
+					fileClose(file)
+					sha = hash("sha1","blob " .. size .. "\0" ..text)
+				end
+				if sha ~= fileHash[path] then
+					outputDebugString("[NativeUI]Update Required: ("..path..")")
+					table.insert(preUpdate,path)
+				end
+			end
+		end
+	end
+	DownloadFiles()
 end
 
+function DownloadFiles()
+	UpdateCount = UpdateCount + 1
+	if not preUpdate[UpdateCount] then
+		DownloadFinish()
+		return
+	end
+	outputDebugString("[NativeUI]Requesting ("..UpdateCount.."/"..(#preUpdate or "Unknown").."): "..tostring(preUpdate[UpdateCount]).."")
+	fetchRemote("https://raw.githubusercontent.com/Allerek/MTASA-NativeUI/master/"..preUpdate[UpdateCount],function(data,err,path)
+		if err == 0 then
+			local size = 0
+			if fileExists(path) then
+				local file = fileOpen(path)
+				size = fileGetSize(file)
+				fileClose(file)
+				fileDelete(path)
+			end
+			local file = fileCreate(path)
+			fileWrite(file,data)
+			local newsize = fileGetSize(file)
+			fileClose(file)
+			outputDebugString("[NativeUI]File Got ("..UpdateCount.."/"..#preUpdate.."): "..path.." [ "..size.."B -> "..newsize.."B ]")
+		else
+			outputDebugString("[NativeUI]Download Failed: "..path.." ("..err..")!",2)
+		end
+		if preUpdate[UpdateCount+1] then
+			DownloadFiles()
+		else
+			DownloadFinish()
+		end
+	end,"",false,preUpdate[UpdateCount])
+end
 
+function DownloadFinish()
+	outputDebugString("[NativeUI]Changing Config File")
+	local file = fileCreate("update.cfg")
+	fileWrite(file,tostring(RemoteVersion))
+	fileClose(file)
+	outputDebugString("[NativeUI]Update Complete (Updated "..#preUpdate.." Files)")
+	outputDebugString("[NativeUI]Please Restart NativeUI")
+	outputChatBox("[NativeUI]Update Complete (Updated "..#preUpdate.." Files)",root,0,255,0)
+	preUpdate = {}
+	preUpdateCount = 0
+	UpdateCount = 0
+	FetchCount = 0
+	preFetch = 0
+end
 
-
-
+addCommandHandler("nativever",function(pla,cmd)
+	local vsdd
+	if fileExists("update.cfg") then
+		local file = fileOpen("update.cfg")
+		local vscd = fileRead(file,fileGetSize(file))
+		fileClose(file)
+		vsdd = tonumber(vscd)
+		if vsdd then
+			outputDebugString("[NativeUI]Version: "..vsdd,3)
+		else
+			outputDebugString("[NativeUI]Version State is damaged! Please use /updatedgs to update",1)
+		end
+	else
+		outputDebugString("[NativeUI]Version State is damaged! Please use /updatedgs to update",1)
+	end
+	if getPlayerName(pla) ~= "Console" then
+		if vsdd then
+			outputChatBox("[NativeUI]Version: "..vsdd,pla,0,255,0)
+		else
+			outputChatBox("[NativeUI]Version State is damaged! Please use /updatedgs to update",pla,255,0,0)
+		end
+	end
+end)
